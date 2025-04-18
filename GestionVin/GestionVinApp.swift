6b06df0abbff2395bc29552b17a0fdd5445d93c7
Puroguramu <yanniscarrieres@gmail.com>
2025-04-18 09:17:18 +0200

    Initial Commit

---
+++ GestionVin/GestionVinApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestionVinApp.swift
+//  GestionVin
+//
+//  Created by Stagiaire on 18/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct GestionVinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
